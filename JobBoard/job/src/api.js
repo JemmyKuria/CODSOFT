a0ccbcc69308b05100a042d08b10716bc11abfc0
JemmyKuria <kuriajemmy@gmail.com>
2024-02-28 07:51:53 +0300

    Defined API endpoints

--- JobBoard/job/src/api.js
+++ JobBoard/job/src/api.js
@@ -5,9 +5,11 @@ const api = axios.create({
 });
 
 export default {
+  // Define your API endpoints here, for example:
   getJobs: () => api.get('/jobs'),
   getJobById: (id) => api.get(`/jobs/${id}`),
   createJob: (job) => api.post('/jobs', job),
   updateJob: (id, job) => api.put(`/jobs/${id}`, job),
   deleteJob: (id) => api.delete(`/jobs/${id}`),
+  applyJob: (jobId, applicationData) => api.post(`/jobs/${jobId}/apply`, applicationData),
 };
